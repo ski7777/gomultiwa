1f7210823666db1f4293465678a4f24ea8447d7c
Raphael Jacob <r.jacob2002@gmail.com>
2019-09-01 13:20:30 +0200

    cleanup

--- internal/waclient/waclient.go
+++ internal/waclient/waclient.go
@@ -3,9 +3,7 @@ package waclient
 import (
 	"time"
 
-	"github.com/Baozisoftware/qrcode-terminal-go"
 	wa "github.com/Rhymen/go-whatsapp"
-	"github.com/google/uuid"
 )
 
 const (
@@ -29,23 +27,4 @@ func NewWAClient(session *wa.Session) (*WAClient, error) {
 		gmw.session = sess
 	}
 	return gmw, err
-}
-
-func RegisterNewClient(c WAClients) (*uuid.UUID, error) {
-	id, _ := uuid.NewRandom()
-	wac, err := wa.NewConn(5 * time.Second)
-	if err != nil {
-		return nil, err
-	}
-	qr := make(chan string)
-	go func() {
-		terminal := qrcodeTerminal.New()
-		terminal.Get(<-qr).Print()
-	}()
-	session, err := wac.Login(qr)
-	if err != nil {
-		return nil, err
-	}
-	c.Clients[id.String()] = newWAClientConfig(&session)
-	return &id, nil
 }
