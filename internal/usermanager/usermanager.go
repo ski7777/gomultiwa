3db4182214008e4d8e793a7e56d9a02637cbfaac
Raphael Jacob <r.jacob2002@gmail.com>
2019-09-06 12:12:16 +0200

    Usermanager: Add SetUserPW func

--- internal/usermanager/usermanager.go
+++ internal/usermanager/usermanager.go
@@ -1,6 +1,9 @@
 package usermanager
 
 import (
+	"errors"
+
+	"github.com/google/uuid"
 	"github.com/ski7777/gomultiwa/internal/config"
 	"github.com/ski7777/gomultiwa/internal/user"
 	"github.com/ski7777/gomultiwa/internal/waclient"
@@ -30,9 +33,22 @@ func (um *UserManager) CreateUser(name string, mail string) (string, error) {
 	return id, nil
 }
 
+func (um *UserManager) SetUserPW(id string, pw string) error {
+	for n := range *um.Userconfig.Users {
+		if (*um.Userconfig.Users)[n].ID == id {
+			x := genPWHash(pw)
+			(*um.Userconfig.Users)[n].Password = x
+			return nil
+
+		}
+	}
+	return errors.New("User ID not found")
+}
+
 func NewUserManager(c config.ConfigData) *UserManager {
 	var um = new(UserManager)
 	um.Userconfig = c.Userconfig
 	um.WAClients = c.WAClients
+	//um.Userconfig.Users = append(um.Userconfig.Users, &user.User{[]string{"a", "b"}, []user.UserPermisson{}, false})
 	return um
 }
