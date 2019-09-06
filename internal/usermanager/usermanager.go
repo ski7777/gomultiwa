2961051ec10b7be6bf910da9ab2570952c33de6c
Raphael Jacob <r.jacob2002@gmail.com>
2019-09-06 12:11:44 +0200

    Usermanager: Add CreateUser func

--- internal/usermanager/usermanager.go
+++ internal/usermanager/usermanager.go
@@ -11,6 +11,25 @@ type UserManager struct {
 	WAClients  *waclient.WAClients
 }
 
+func (um *UserManager) CreateUser(name string, mail string) (string, error) {
+	for _, u := range *um.Userconfig.Users {
+		if u.Mail == mail {
+			return "", errors.New("Mailaddress already registered")
+		}
+	}
+	i, err := uuid.NewRandom()
+	if err != nil {
+		return "", err
+	}
+	id := i.String()
+	var u = new(user.User)
+	u.ID = id
+	u.Name = name
+	u.Mail = mail
+	*um.Userconfig.Users = append(*um.Userconfig.Users, u)
+	return id, nil
+}
+
 func NewUserManager(c config.ConfigData) *UserManager {
 	var um = new(UserManager)
 	um.Userconfig = c.Userconfig
