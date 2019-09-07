b922da78a278263085d7205fc8e02ab6af705a79
Raphael Jacob <r.jacob2002@gmail.com>
2019-09-07 17:22:37 +0200

    Add plenty new funcs...

--- internal/usermanager/usermanager.go
+++ internal/usermanager/usermanager.go
@@ -2,6 +2,7 @@ package usermanager
 
 import (
 	"errors"
+	"regexp"
 	"sync"
 
 	"github.com/google/uuid"
@@ -9,6 +10,8 @@ import (
 	"github.com/ski7777/gomultiwa/internal/user"
 	"github.com/ski7777/gomultiwa/internal/waclient"
 )
+
+var rxEmail = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 
 type UserManager struct {
 	Userconfig     *user.Users
@@ -57,6 +60,92 @@ func (um *UserManager) CheckUserPW(id string, pw string) (bool, error) {
 		return (*um.Userconfig.Users)[n].Password == genPWHash(pw), nil
 	}
 }
+func (um *UserManager) SetUserName(id string, name string) error {
+	um.userconfiglock.Lock()
+	defer um.userconfiglock.Unlock()
+	if n, err := um.getUserIndexByID(id); err != nil {
+		return err
+	} else {
+		(*um.Userconfig.Users)[n].Name = name
+		return nil
+	}
+}
+func (um *UserManager) SetUserMail(id string, mail string) error {
+	if len(mail) > 254 || !rxEmail.MatchString(mail) {
+		return errors.New("Mailaddress not valid")
+	}
+	um.userconfiglock.Lock()
+	defer um.userconfiglock.Unlock()
+	if n, err := um.getUserIndexByID(id); err != nil {
+		return err
+	} else {
+		(*um.Userconfig.Users)[n].Mail = mail
+		return nil
+	}
+}
+func (um *UserManager) SetUserAdmin(id string, admin bool) error {
+	um.userconfiglock.Lock()
+	defer um.userconfiglock.Unlock()
+	if n, err := um.getUserIndexByID(id); err != nil {
+		return err
+	} else {
+		(*um.Userconfig.Users)[n].Admin = admin
+		return nil
+	}
+}
+func (um *UserManager) GetUserAdmin(id string) (bool, error) {
+	um.userconfiglock.Lock()
+	defer um.userconfiglock.Unlock()
+	if n, err := um.getUserIndexByID(id); err != nil {
+		return false, err
+	} else {
+		return (*um.Userconfig.Users)[n].Admin, nil
+	}
+}
+func (um *UserManager) AddUserClient(id string, client string) error {
+	if _, ok := um.WAClients.Clients[client]; !ok {
+		return errors.New("Client ID invalid")
+	}
+	um.userconfiglock.Lock()
+	defer um.userconfiglock.Unlock()
+	if n, err := um.getUserIndexByID(id); err != nil {
+		return err
+	} else {
+		for i := 0; i < len(*(*um.Userconfig.Users)[n].Clients); i++ {
+			if (*(*um.Userconfig.Users)[n].Clients)[i] == id {
+				return errors.New("Client ID already associated with user")
+			}
+		}
+		*(*um.Userconfig.Users)[n].Clients = append((*(*um.Userconfig.Users)[n].Clients), client)
+	}
+	return nil
+}
+
+func (um *UserManager) DeleteUserClient(id string, client string) error {
+	um.userconfiglock.Lock()
+	defer um.userconfiglock.Unlock()
+	if n, err := um.getUserIndexByID(id); err != nil {
+		return err
+	} else {
+		for i := 0; i < len(*(*um.Userconfig.Users)[n].Clients); i++ {
+			if (*(*um.Userconfig.Users)[n].Clients)[i] == client {
+				*(*um.Userconfig.Users)[n].Clients = append((*(*um.Userconfig.Users)[n].Clients)[:i], (*(*um.Userconfig.Users)[n].Clients)[i+1:]...)
+				return nil
+			}
+		}
+		return errors.New("Client ID not associated with user")
+	}
+}
+
+func (um *UserManager) GetUserClients(id string) (*[]string, error) {
+	um.userconfiglock.Lock()
+	defer um.userconfiglock.Unlock()
+	if n, err := um.getUserIndexByID(id); err != nil {
+		return nil, err
+	} else {
+		return (*um.Userconfig.Users)[n].Clients, nil
+	}
+}
 
 func (um *UserManager) GetUserIDByMail(mail string) (string, error) {
 	um.userconfiglock.Lock()
@@ -83,6 +172,5 @@ func NewUserManager(c config.ConfigData) *UserManager {
 	var um = new(UserManager)
 	um.Userconfig = c.Userconfig
 	um.WAClients = c.WAClients
-	//um.Userconfig.Users = append(um.Userconfig.Users, &user.User{[]string{"a", "b"}, []user.UserPermisson{}, false})
 	return um
 }
