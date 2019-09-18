f3985c9f812a99fa87382c7cda6e1fc567804a19
Raphael Jacob <r.jacob2002@gmail.com>
2019-09-18 23:53:50 +0200

    Comment exit.go

--- internal/gomultiwa/shell/commands/exit.go
+++ internal/gomultiwa/shell/commands/exit.go
@@ -3,6 +3,7 @@ package commands
 import "github.com/abiosoft/ishell"
 import gmwi "github.com/ski7777/gomultiwa/internal/gomultiwa/interface"
 
+// GetCmdExit returns the command to exit gomultiwa
 func GetCmdExit(gmw gmwi.GoMultiWAInterface) *ishell.Cmd {
 	return &ishell.Cmd{
 		Name: "exit",
