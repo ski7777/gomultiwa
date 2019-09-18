d0387f0ed500c99880d5263c9df030857b1d813a
Raphael Jacob <r.jacob2002@gmail.com>
2019-09-18 23:56:20 +0200

    Comment user/config.go

--- internal/user/config.go
+++ internal/user/config.go
@@ -1,9 +1,11 @@
 package user
 
+// Users represents a list of all users
 type Users struct {
 	Users *[]*User `json:"users"`
 }
 
+// NewUsers returns a new Users struct
 func NewUsers() *Users {
 	u := new(Users)
 	u.Users = &[]*User{}
