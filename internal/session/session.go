58efbe0ee5db51335bb286271c02a13c572da3e9
Raphael Jacob <r.jacob2002@gmail.com>
2019-09-18 23:54:22 +0200

    Comment session.go

--- internal/session/session.go
+++ internal/session/session.go
@@ -5,6 +5,7 @@ import (
 	"time"
 )
 
+// Session represents a login session with user id, and many timestamps
 type Session struct {
 	created int64
 	lastuse int64
@@ -12,6 +13,7 @@ type Session struct {
 	user    string
 }
 
+// NewSession returns a new Session struct
 func NewSession(timeout int64, user string) *Session {
 	s := new(Session)
 	s.timeout = timeout
@@ -20,14 +22,17 @@ func NewSession(timeout int64, user string) *Session {
 	return s
 }
 
+// IsValid returns whether a Session is still valid
 func (s *Session) IsValid() bool {
 	return s.created+s.timeout <= time.Now().UnixNano()
 }
 
+// GetUserID returns the user id of the session
 func (s *Session) GetUserID() string {
 	return s.user
 }
 
+// Use saves the current timestamp to the Session´s lastuse timestamp
 func (s *Session) Use() error {
 	if !s.IsValid() {
 		return errors.New("Session invalid")
