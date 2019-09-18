4a69dcd76fdf23380d69d4b5b615d6cb594489e4
Raphael Jacob <r.jacob2002@gmail.com>
2019-09-18 23:54:07 +0200

    Comment save.go

--- internal/gomultiwa/shell/commands/save.go
+++ internal/gomultiwa/shell/commands/save.go
@@ -3,6 +3,7 @@ package commands
 import "github.com/abiosoft/ishell"
 import gmwi "github.com/ski7777/gomultiwa/internal/gomultiwa/interface"
 
+// GetCmdSave returns the command to save the config
 func GetCmdSave(gmw gmwi.GoMultiWAInterface) *ishell.Cmd {
 	return &ishell.Cmd{
 		Name: "save",
