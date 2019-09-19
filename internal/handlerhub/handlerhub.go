04e8606c7d5296c8fe2a2424ada068f056ed7d4c
Raphael Jacob <r.jacob2002@gmail.com>
2019-09-19 13:16:08 +0200

    Comment handlerhub.go

--- internal/handlerhub/handlerhub.go
+++ internal/handlerhub/handlerhub.go
@@ -4,14 +4,32 @@ import (
 	whatsapp "github.com/Rhymen/go-whatsapp"
 )
 
+// HandlerHub implements all message handlers
 type HandlerHub struct{}
 
-func (hh *HandlerHub) HandleError(err error, id string)                                          {}
-func (hh *HandlerHub) HandleTextMessage(message whatsapp.TextMessage, id string)                 {}
-func (hh *HandlerHub) HandleImageMessage(message whatsapp.ImageMessage, id string)               {}
-func (hh *HandlerHub) HandleDocumentMessage(message whatsapp.DocumentMessage, id string)         {}
-func (hh *HandlerHub) HandleVideoMessage(message whatsapp.VideoMessage, id string)               {}
-func (hh *HandlerHub) HandleAudioMessage(message whatsapp.AudioMessage, id string)               {}
-func (hh *HandlerHub) HandleLocationMessage(message whatsapp.LocationMessage, id string)         {}
+// HandleError handles type error
+func (hh *HandlerHub) HandleError(err error, id string) {}
+
+// HandleTextMessage handles type whatsapp.TextMessage
+func (hh *HandlerHub) HandleTextMessage(message whatsapp.TextMessage, id string) {}
+
+// HandleImageMessage handles type whatsapp.ImageMessage
+func (hh *HandlerHub) HandleImageMessage(message whatsapp.ImageMessage, id string) {}
+
+// HandleDocumentMessage handles type whatsapp.DocumentMessage
+func (hh *HandlerHub) HandleDocumentMessage(message whatsapp.DocumentMessage, id string) {}
+
+// HandleVideoMessage handles type erwhatsapp.VideoMessageror
+func (hh *HandlerHub) HandleVideoMessage(message whatsapp.VideoMessage, id string) {}
+
+// HandleAudioMessage handles type whatsapp.AudioMessage
+func (hh *HandlerHub) HandleAudioMessage(message whatsapp.AudioMessage, id string) {}
+
+// HandleLocationMessage handles type whatsapp.LocationMessage
+func (hh *HandlerHub) HandleLocationMessage(message whatsapp.LocationMessage, id string) {}
+
+// HandleLiveLocationMessage handles type whatsapp.LiveLocationMessage
 func (hh *HandlerHub) HandleLiveLocationMessage(message whatsapp.LiveLocationMessage, id string) {}
-func (hh *HandlerHub) HandleJsonMessage(message string, id string)                               {}
+
+// HandleJsonMessage handles type string (JSON)
+func (hh *HandlerHub) HandleJsonMessage(message string, id string) {}
